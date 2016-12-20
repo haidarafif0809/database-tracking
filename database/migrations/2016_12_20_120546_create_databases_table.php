16fccb58e2ea58d608acfa43dff89232cd627d86
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2016-12-20 19:43:14 +0700

    Initial Commit

---
+++ database/migrations/2016_12_20_120546_create_databases_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDatabasesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('databases', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nama_database');
+          $table->string('keterangan')->nullable();
+            $table->date('tanggal');
+            $table->integer('id_user');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('databases');
+    }
+}
---
+++ database/migrations/2016_12_20_120613_create_columns_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateColumnsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('columns', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nama_column');
+            $table->integer('id_user');
+            $table->integer('id_table');
+            $table->string('keterangan')->nullable();
+            $table->date('tanggal');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('columns');
+    }
+}
